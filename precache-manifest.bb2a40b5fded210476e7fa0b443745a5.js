8b83de26946bf1c32b001a4b8ff3241a1f05f961
adadachao <adadachao@gmail.com>
2025-08-18 18:46:13 +0800

    Add files via upload
    up

---
+++ precache-manifest.bb2a40b5fded210476e7fa0b443745a5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d21cc8276d30dcf2c9c116a1a1b40e26",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d0a111b22c02fde10876",
+    "url": "./static/js/2.00b5474e.chunk.js"
+  },
+  {
+    "revision": "d255601c39ba9ea8be3f576f2fb7e1d5",
+    "url": "./static/js/2.00b5474e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "126ac727aa9b8803cfd9",
+    "url": "./static/js/main.02c53576.chunk.js"
+  },
+  {
+    "revision": "51cca0025286cb519c68",
+    "url": "./static/js/runtime-main.390e6a21.js"
+  },
+  {
+    "revision": "94e173deb0980ec7ee72b38d7378e8ff",
+    "url": "./static/media/logo.94e173de.svg"
+  },
+  {
+    "revision": "29e5e4e67ebbf4f27bffa2fc840d68dc",
+    "url": "./static/media/logo_white.29e5e4e6.svg"
+  },
+  {
+    "revision": "17391abb107005d92aef3cdd637881a9",
+    "url": "./static/media/mova.17391abb.png"
+  },
+  {
+    "revision": "2e55bf0316383e46b87d4d47606d5a26",
+    "url": "./static/media/wordmark_white.2e55bf03.svg"
+  }
+]);
